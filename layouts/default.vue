6f7f847be650ce166191dc1ec201292db1b60da7
Sam <samcparker1999@gmail.com>
2020-07-23 16:35:34 +0100

    Added nav draw for mobile devices

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,16 +3,47 @@
     <v-app-bar style="background-color: rgb(0, 0, 0, 0);" class="hoveropacity" tile elevation="0" fixed inverted-scroll >
       <a href="#">
         <v-img style="max-width: 50px; max-height: 50px"  src="/logo.png"></v-img>
-
       </a>
       <!-- <v-toolbar-title class="tb pl-4" >Sam C. Parker</v-toolbar-title> -->
       <v-spacer></v-spacer>
       <!-- <a href="#work" class="white--text">My Work</a>
       <a href="#contact" class="white--text pl-4">Contact Me</a> -->
-      <v-btn href="#work" tile outlined style="width: 150px" small>My Work</v-btn>
-      <v-btn href="#contact" tile outlined style="width: 150px" class="ml-5" small>Contact Me</v-btn>
-      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+      <div class="hidden-sm-and-down">
+        <v-btn href="#work" tile outlined style="width: 150px" small>My Work</v-btn>
+        <v-btn href="#contact" tile outlined style="width: 150px" class="ml-5" small>Contact Me</v-btn>
+      </div>
+      <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
     </v-app-bar>
+    <v-navigation-drawer v-model="drawer" right dark app color="#121212">
+      <v-list>
+        <v-list-item class="mt-5" link href="#work">
+          <v-list-item-avatar tile>
+              <img src="/logo.png">
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title class="titled">
+                Sam C. Parker
+              </v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
+        <v-spacer class="my-5"></v-spacer>
+        <v-list-item link href="#work">
+          <v-list-item-content>
+            <v-list-item-title class="sub">
+              My Work
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link href="#contact">
+          <v-list-item-content>
+            <v-list-item-title  class="sub">
+              Contact Me
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+    </v-navigation-drawer>
     <v-main>
       <nuxt />
     </v-main>
@@ -21,7 +52,11 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      drawer: false
+    }
+  }
 }
 </script>
 
@@ -68,5 +103,15 @@ export default {
     font-size: 30px;
   }
 
+  .titled {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 300;
+    font-size: 20px;
+  }
 
+  .sub {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 500;
+    font-size: 30px;
+  }
 </style>
